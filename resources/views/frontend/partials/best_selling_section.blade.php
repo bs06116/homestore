a0b8c6f4aedf08e7d513bc4896418e8017aa164a
bs06116 <iasimriaz@gmail.com>
2021-06-04 13:01:28 +0500

    style on front pages

--- resources/views/frontend/partials/best_selling_section.blade.php
+++ resources/views/frontend/partials/best_selling_section.blade.php
@@ -8,7 +8,7 @@
                     </h3>
                     <a href="javascript:void(0)" class="ml-auto mr-0 btn btn-primary btn-sm shadow-md">{{ translate('Top 20') }}</a>
                 </div>
-                <div class="aiz-carousel gutters-10 half-outside-arrow" data-items="6" data-xl-items="5" data-lg-items="4"  data-md-items="3" data-sm-items="2" data-xs-items="2" data-arrows='true' data-infinite='true'>
+                <div class="aiz-carousel gutters-10 half-outside-arrow" data-items="7" data-xl-items="6" data-lg-items="5"  data-md-items="4" data-sm-items="3" data-xs-items="3" data-arrows='true' data-infinite='true'>
                     @foreach (filter_products(\App\Product::where('published', 1)->orderBy('num_of_sale', 'desc'))->limit(12)->get() as $key => $product)
                         <div class="carousel-box">
                             <div class="aiz-card-box border border-light rounded hov-shadow-md my-2 has-transition">
@@ -16,7 +16,7 @@
                                     <a href="{{ route('product', $product->slug) }}" class="d-block">
                                         <img
                                         class="img-fit lazyload mx-auto h-120px h-md-150px "
-                                      style="width: 90% ; margin-top: 8px"
+                                        style="width: 90% ; margin-top: 8px ; border-radius: 5px 5px 0px 0px ;  "
                                             src="{{ static_asset('assets/img/placeholder.jpg') }}"
                                             data-src="{{ uploaded_asset($product->thumbnail_img) }}"
                                             alt="{{  $product->getTranslation('name')  }}"
